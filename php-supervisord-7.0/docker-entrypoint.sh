4c0f2f84e1e9d3461f7567c5d0e733026e5f5582
melon <melon198502@gmail.com>
2016-10-18 18:50:04 +0800

    fix the bug of failing to create the custom php config file(docker-php-app-custom.ini)

--- php-supervisord-7.0/docker-entrypoint.sh
+++ php-supervisord-7.0/docker-entrypoint.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 set -e
 
-if [ -n "$PHP_CUSTOM_CONFIG" ];then
-    echo "$PHP_CUSTOM_CONFIG" > /usr/local/etc/php/conf.d/docker-php-app-custom.ini
-fi 
-
 # first arg is `-c` or `--some-option`
 if [ "${1#-}" != "$1" ]; then
 	set -- supervisord "$@"
@@ -12,7 +8,12 @@ fi
 
 # allow the container to be started with www-data
 if [ "$1" = 'supervisord' -a "$(id -u)" = '0' ]; then
+    if [ -n "$PHP_CUSTOM_CONFIG" ];then
+        echo "$PHP_CUSTOM_CONFIG" > /usr/local/etc/php/conf.d/docker-php-app-custom.ini
+    fi
+
     chown -R www-data:www-data /www/log/jobs /www/web/queue
     exec gosu www-data "$0" "$@"
 fi
+
 exec "$@"
